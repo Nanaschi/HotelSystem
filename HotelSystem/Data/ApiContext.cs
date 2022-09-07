727428bc1743f0a8e8475cc4914880459d00734c
Daniel <danil.e@ironwill-et.com>
2022-09-07 10:37:45 +0300

    Initial commit

---
+++ HotelSystem/Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using HotelSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelSystem.Data;
+
+public class ApiContext: DbContext
+{
+    public DbSet<HotelBooking> Bookings { get; set; }
+
+    public ApiContext(DbContextOptions<ApiContext> dbContextOptions)
+    :base (dbContextOptions)
+    {
+        
+    }
+}
---
+++ HotelSystem/Models/HotelBooking.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelSystem.Models;
+
+public class HotelBooking
+{
+    public int ID { get; set; }
+    public int RoomNumber { get; set; }
+    public string? ClientName { get; set; }
+}
